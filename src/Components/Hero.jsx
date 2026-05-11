ad59e8738f1502705cd390fdee57a9564032c7b0
Ayush Yadav <ayushydv241@gmail.com>
2026-05-11 12:41:03 +0530

    First commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div>This is hero section</div>
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed corporis veniam voluptates laboriosam tempora commodi deserunt consequatur molestiae saepe ipsa ea eum, dolorum laudantium quis incidunt quibusdam illum temporibus, quos quia. Nemo magnam optio quasi fugiat beatae. Quo provident tempora iusto asperiores reiciendis veritatis nihil est, dolor perspiciatis dignissimos voluptate.</div>
+  <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt earum eius iste delectus eaque ab facere obcaecati iure inventore natus quasi, facilis quae totam nostrum eligendi eum. Officia, praesentium soluta in quam sapiente dolor pariatur ab laboriosam debitis nisi, cupiditate aspernatur laborum at consequuntur architecto dolores ullam odit nam tenetur, labore inventore quia! Porro debitis aliquam id delectus. Molestiae beatae possimus at? Animi voluptate veniam ipsum cum est obcaecati ratione eos culpa expedita, distinctio porro, maxime velit magni aut repudiandae quidem odit quia temporibus dolore sed unde quam. Temporibus soluta sint, illum accusantium architecto voluptatum dolores laboriosam veniam asperiores. Ex!</div>
+  <div>Yadav </div>
+  <div>Hello</div>
+  <div>Hello</div>
+  <div>Yadav</div>
+  <div>Hello</div>
+  <div>Hello</div>
+  <div>Hello</div>
+  <div>Class</div>
+  <div>Based</div>
+  </>
+  )
+}
+
+export default Hero
